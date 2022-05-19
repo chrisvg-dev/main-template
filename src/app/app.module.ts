ab79f1800ab345708de9a869c7ed2a825a069fb1
CRISTHIAN VILLEGAS <cristianvg9692@gmail.com>
2022-05-18 19:31:13 -0500

    update may 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -57,7 +59,8 @@ import { ConstructionComponent } from './shared/components/construction/construc
     SpinnerModule
   ],
   providers: [ 
-    { provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi: true } 
+    { provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi: true },
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ],
   bootstrap: [AppComponent]
 })
